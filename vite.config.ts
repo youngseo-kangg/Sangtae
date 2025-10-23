0aff5ccd8af174906c2eb29336a6e911070e6e7b
Youngseo Kang <youngseo.kangg@gmail.com>
2025-10-23 15:47:59 +0900

    chore: fix package.json and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,35 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import type { UserConfig } from "vite";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ command }): UserConfig => {
-	if (command === "build") {
-		return {
-			plugins: [react()],
-			build: {
-				outDir: "lib",
-				lib: {
-					entry: "src/index.ts",
-					name: "Sangtae",
-					formats: ["cjs"],
-					fileName: "index",
-				},
-				minify: false,
-				rollupOptions: {
-					external: ["react", "react-dom"],
-					output: {
-						globals: {
-							react: "React",
-							"react-dom": "ReactDOM",
-						},
-					},
-				},
-			},
-		};
-	} else {
-		return {
-			plugins: [react()],
-		};
-	}
+  if (command === "build") {
+    return {
+      plugins: [react()],
+      build: {
+        outDir: "lib",
+        lib: {
+          entry: path.resolve(__dirname, "src/index.ts"),
+          name: "Sangtae",
+          formats: ["cjs"],
+          fileName: "index",
+        },
+        minify: false,
+        rollupOptions: {
+          external: ["react", "react-dom"],
+          output: {
+            globals: {
+              react: "React",
+              "react-dom": "ReactDOM",
+            },
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      plugins: [react()],
+    };
+  }
 });
